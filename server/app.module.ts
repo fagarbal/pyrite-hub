6f678531d0e34fd3aa2ed3a9065e1c1ee12daefc
Fabio Garcia <fagarbal@gmail.com>
2018-05-08 15:54:30 +0200

    remove db

--- server/app.module.ts
+++ server/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ComponentsModule } from './component/components.module';
-import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './auth/auth.module';
 import { AppController } from './app.controller';
 

67c4ef648dff5822d8de494a8dcacc0766da3ca5
fagarbal <fagarbal@gmail.com>
2018-05-10 14:42:07 +0200

    added meta description

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,7 +12,8 @@ export default class MyDocument extends Document {
             <html>
                 <Head>
                     <meta charSet="utf-8" />
-                    <title>pyriteHub</title>
+                    <title>PyriteHub</title>
+                    <meta name="description" content="From your git repository to internet with some clicks"></meta>
                     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"></meta>
                     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossOrigin="anonymous"></link>
                     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossOrigin="anonymous"></link>

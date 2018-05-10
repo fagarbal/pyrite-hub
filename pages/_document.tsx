939de50c862b26ab01ae610d20bb07055623194f
fagarbal <fagarbal@gmail.com>
2018-05-10 14:15:08 +0200

    added favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,6 +17,7 @@ export default class MyDocument extends Document {
                     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossOrigin="anonymous"></link>
                     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossOrigin="anonymous"></link>
                     <link rel="stylesheet" href="/static/styles/styles.css"></link>
+                    <link rel="shortcut icon" type="image/png" href="/static/favicon.png"/>
                 </Head>
                 <body className="custom_class">
                     <Main />

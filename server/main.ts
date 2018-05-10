0ca1120fb030516728acf438ab66245d404719f7
fagarbal <fagarbal@gmail.com>
2018-05-10 14:20:31 +0200

    added compression middleware

--- server/main.ts
+++ server/main.ts
@@ -4,11 +4,13 @@ import * as session from 'express-session';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { initPassport } from './passport.init';
+import * as compression from 'compression';
 
 async function bootstrap() {
 	dotenv.load();	
 
 	const app = await NestFactory.create(AppModule);
+	app.use(compression());
 
 	app.use(session({
 		secret: process.env.SESSION_SECRET,

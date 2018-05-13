613b6ff1228514d76e2d37063a241c06f2bcc996
fagarbal <fagarbal@gmail.com>
2018-05-13 21:05:47 +0200

    Changed apollo configuration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,25 +1,15 @@
-import React from 'react'
-import App, { Container } from 'next/app'
+import React from 'react';
+import App, { Container } from 'next/app';
+import apolloClient from "../public/lib/initApollo";
 import { ApolloProvider } from 'react-apollo';
-import { ApolloClient } from 'apollo-client';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import fetch from 'node-fetch';
-import { createHttpLink } from 'apollo-link-http';
-
-const link = createHttpLink({ uri: process.env.GRAPHQL_URL || (location.href + 'graphql'), fetch, headers: { compression: "no"}});
-
-const client = new ApolloClient({
-    link,
-    cache: new InMemoryCache(),
-});
 
 export default class MyApp extends App {
     props: any;
     render() {
-        const { Component, pageProps } = this.props
+        const { Component, pageProps } = this.props;
         return (
             <Container>
-                <ApolloProvider client={client}>
+                <ApolloProvider client={apolloClient()}>
                     <Component {...pageProps} />
                 </ApolloProvider>
             </Container>
---
+++ public/lib/initApollo.ts
@@ -0,0 +1,35 @@
+import { ApolloClient } from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import fetch from "isomorphic-unfetch";
+
+let apolloClient = null;
+
+if (!(<any>process).browser) {
+  (<any>global).fetch = fetch;
+}
+
+function create() {
+  return new ApolloClient({
+    connectToDevTools: (<any>process).browser,
+    ssrMode: !(<any>process).browser,
+    link: new HttpLink({
+      uri: process.env.GRAPHQL_URL || (location.href + 'graphql'),
+      fetch,
+      headers: { compression: "no" }
+    }),
+    cache: new InMemoryCache()
+  });
+}
+
+export default function initApollo() {
+  if (!(<any>process).browser) {
+    return create();
+  }
+
+  if (!apolloClient) {
+    apolloClient = create();
+  }
+
+  return apolloClient;
+}

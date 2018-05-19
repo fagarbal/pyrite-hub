4122ce79a32d678bc3adc03c2527680269fb33a7
fagarbal <fagarbal@gmail.com>
2018-05-19 14:14:22 +0200

    Added sigin popup

--- public/components/header/header.template.tsx
+++ public/components/header/header.template.tsx
@@ -7,18 +7,39 @@ import { translation } from '@utils/i18n';
 export default function (this: Header) {
     return (
         <div className="header">
-            <Modal isOpen={this.state.modal} centered={true} backdrop="static">
-                <ModalHeader>{translation('header.loginMethod')}</ModalHeader>
-                <ModalBody className="m-auto">
-                    <a className="btn btn-secondary" href="/auth/github">
-                        Github <i className="fab fa-github"></i>
-                    </a>
-                    <a className="btn btn-secondary ml-3 mr-3" href="/auth/gitlab">
-                        Gitlab <i className="fab fa-gitlab"></i>
-                    </a>
-                    <a className="btn btn-secondary" href="/auth/bitbucket">
-                        Bitbucket <i className="fab fa-bitbucket"></i>
-                    </a>
+            <Modal isOpen={this.state.modal} centered={true} backdrop="static" fade={false}>
+                <ModalHeader>{translation('header.signinTitle')}</ModalHeader>
+                <ModalBody className="ml-4 mr-4">
+                    <form>
+                        <div className="form-group">
+                            <label><b>{translation('header.username')}</b></label>
+                            <input type="email" className="form-control" required/>
+                        </div>
+                        <div className="form-group">
+                            <label><b>{translation('header.password')}</b></label>
+                            <a className="float-right" href="">{translation('header.forgot')}</a>
+                            <input type="password" className="form-control" required/>
+                        </div>
+
+                        <div className="col-sm-4 offset-sm-4">
+                            <button type="submit" className="btn btn-primary btn-block">{translation('header.signin')}</button>
+                        </div>
+
+                        <hr/>
+                        <p className="text-center">{translation('header.or')}</p>
+                        <hr/>
+
+                        <a className="btn btn-outline-dark btn-block" href="/auth/github">
+                            <i className="fab fa-github"></i><span> {translation('header.signinWith')} Github</span>
+                        </a>
+                        <a className="btn btn-outline-dark btn-block" href="/auth/gitlab">
+                            <i className="fab fa-gitlab"></i><span> {translation('header.signinWith')} Gitlab</span>
+                        </a>
+                        <a className="btn btn-outline-dark btn-block" href="/auth/bitbucket">
+                            <i className="fab fa-bitbucket"></i><span> {translation('header.signinWith')} Bitbucket</span>
+                        </a>
+                    </form>
+
                 </ModalBody>
                 <ModalFooter>
                     <Button color="danger" onClick={this.toggleModal}>{translation('cancel')}</Button>
@@ -30,7 +51,7 @@ export default function (this: Header) {
                 </NavbarBrand>
                     <Nav className="ml-auto" navbar>
                         <NavItem>
-                            <button type="button" className="btn btn-primary" onClick={this.toggleModal}>{translation('header.login')}</button>
+                            <button type="button" className="btn btn-primary" onClick={this.toggleModal}>{translation('header.signin')}</button>
                         </NavItem>
                     </Nav>
             </Navbar>
